1a5842c54b8b3b65f62ba624b3375461c1957afa
jimmy <hgw900109@163.com>
2015-10-20 10:35:10 +0800

    集成左滑功能

--- app/src/main/java/com/jimmy/ordersystem/MainActivity.java
+++ app/src/main/java/com/jimmy/ordersystem/MainActivity.java
@@ -3,11 +3,23 @@ package com.jimmy.ordersystem;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.slidingmenu.lib.SlidingMenu;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setBehindWidth(200);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.activity_main);
     }
 }
